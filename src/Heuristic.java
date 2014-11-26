a5c38c01d57b6c5091f437f51ec04c42c28de3e8
Alex <alex.hunter14@btinternet.com>
2014-11-26 00:25:58 +0000

    finished genex(), and added namegen()
    genex() function now generates one exercise. Main function now prints
    the names of the specified number of exercises (just proves random
    works).
    namegen() generates a name for the exercise - names might not make
    sense, but we still have names.

--- src/Heuristic.java
+++ src/Heuristic.java
@@ -1,4 +1,4 @@
- 
+ import java.util.Scanner;
 public class Heuristic {
 
 	/**
@@ -7,7 +7,30 @@ public class Heuristic {
 	public static void main(String[] args) 
 	{
 		//Proof of concept code here
+		Scanner scanner1 = new Scanner( System.in );
 		
+
+		int value =1;
+
+		Exercise exercises; 
+		while (value != 0)
+		{	
+			System.out.print("\nMake how many exercises? (0 terminates):\n");
+			value = scanner1.nextInt();
+			for (int i = 0; i < value; i ++)
+			{
+				exercises = genex();
+				System.out.print(exercises.name+"\n");
+				
+				
+			}
+		}
+		
+		//System.out.print(exercises[1].mgroup);
+		
+
+		scanner1.close();
+
 		
 		
 	}
@@ -16,48 +39,96 @@ public class Heuristic {
 	static Exercise genex() 
 	{	
 		//array representing 13 major muscle groups. provisional.
-		String[] allgroups = {"Abdominal", "Biceps", 	
-				"Deltoids", "Erector Spinae", "Gastrocnemius & Soleus", 
-				"Gluteus", "Hamstrings", "Latissimus Dorsi & Rhomboids", 
-				"Obliques", "Pectorals", "Quadriceps", "Trapezius", "Triceps"};
+		String[] allgroups = 
+			{	"Abdominal", 
+				"Biceps", 	
+				"Deltoids", 
+				"Erector Spinae", 
+				"Gastrocnemius & Soleus", 
+				"Gluteus", 
+				"Hamstrings", 
+				"Latissimus Dorsi & Rhomboids", 
+				"Obliques", 
+				"Pectorals", 
+				"Quadriceps", 
+				"Trapezius", 
+				"Triceps"
+			};
 		
-		
-		//temporary array for storing an arbitrary set of exercises
-		Exercise[] tmpexercises;			
-		tmpexercises = new Exercise[39];
-		
-		//counts index of tmpexercises as the function adds exercises to it
-		int count = 0;
-		
-		
-		//these 3 for loops loop through allgroups 3 times, generating an exercise for each
-		//muscle group (13), and looping 3 times (13 x 3 = 39 exercises total)
-		for (int i = 0; i < 3; i++)	
-		{	count = 0;				
-			for (int j = 0; j < allgroups.length; j++)
-			{
-				String[] groups;
-				int elemnum = 10*(int)Math.random();
-				groups = new String[elemnum];
-				for (int k = 0; k < elemnum; k++)
-				{
-					int index = 10*(int)Math.random();
-					while (index >12)
-					{
-						index = (int)Math.random();
-					}
-					
-					groups[k] = allgroups[index];
-					
-					
-				}
-				String exname = allgroups[j];
-				tmpexercises[i * count] = new Exercise(exname, allgroups[j], groups, 10*(int)Math.random(), 'i', 90, 5, 5);
-				count ++;
-			}
+
+
+		Exercise tmpexercise;			
+		String[] groups;
+		int elemnum = (int)(10*Math.random())+(int)(3*Math.random());
+
+		groups = new String[elemnum];
+		for (int k = 0; k < elemnum; k++)
+		{
+			int index = (int)(10*Math.random()) + (int)(3*Math.random());
+			
+			groups[k] = allgroups[index];
 		}
+		
+		String exname = namegen(allgroups[elemnum]);
+		
+		
+		
+		tmpexercise= new Exercise(exname, allgroups[elemnum], groups, 
+				(int)(10*Math.random()), 'i', (int)(100*Math.random()), 
+				(int)(10*Math.random()), (int)(10*Math.random()));
+
+		return(tmpexercise);
 	}
 
+	static String namegen(String name)
+	{
+		String[] names =
+			{
+				"rows",
+				"pulls",
+				"thrusts",
+				"pushes",
+				"crunches",
+				"curls",
+				"raises",
+				"pulldowns",
+				"twists",
+				"jumps"
+			};
+		
+		String[] vowels = {"a", "e", "i", "o", "u" };
+		
+		String newname;
+		
+		int chop = 0;
+		//System.out.print(name.substring(name.length()-1));
+		if ((String)name.substring(name.length()-1) == "s")
+		{
+			chop = 1;
+			System.out.print("YUP");
+			System.out.print(name.substring(name.length()-2, name.length()-1));
+			for (int i = 0; i < 5; i++)
+			{
+				if(name.substring(name.length()-2, name.length()-1) == vowels[i])
+				{
+					chop = 0;
+					break;
+				}
+			}
+		}
+		
+		if (chop == 1)
+		{
+			newname = name.substring(0, name.length()-1) + " " + names[(int)(10*Math.random())];
+		}
+		else
+		{
+			newname = name + " " + names[(int)(10*Math.random())];
+		}
+		return newname;
+		
+		
+	}
 	
 
 }
